d3b35bcecb4c732cf916cf6fba69e6cc1c2c95bd
robinboehm <robin.boehm@workshops.de>
2020-08-21 21:31:42 +0000

    chore(swagger): add swagger

--- bookmonkey-rest-api/src/main.ts
+++ bookmonkey-rest-api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Bookmoney API')
+    .setDescription(`
+    This is your API Playground. 😎
+    `)
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
