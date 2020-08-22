35370f1971dc134cee4648752d827a483d59284e
robinboehm <robin.boehm@workshops.de>
2020-08-22 14:00:13 +0000

    feat(db): add db connection

--- bookmonkey-rest-api/src/app.module.ts
+++ bookmonkey-rest-api/src/app.module.ts
@@ -3,8 +3,19 @@ import { AppController } from './app.controller';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 3306,
+        username: 'gitpod',
+        password: '',
+        database: 'workshop',
+        entities: [],
+        synchronize: true,
+    }),],
     controllers: [AppController, BooksController],
     providers: [BooksService],
 })
