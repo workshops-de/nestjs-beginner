fd57bf28d5a021778f82825b77ac88d42f4e3460
robinboehm <robin.boehm@workshops.de>
2020-08-22 14:46:04 +0000

    feat(books): use repo for books

--- bookmonkey-rest-api/src/books/books.service.ts
+++ bookmonkey-rest-api/src/books/books.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
+import { Book } from './book.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 @Injectable()
 export class BooksService {
 
-    private books: string[] = [
-        'NestJS for Beginner',
-        'A cats guide',
-        'Running with cats'
-    ]
-
-    public getAll() {
-        return this.books;
+    constructor(@InjectRepository(Book)
+    private booksRepository: Repository<Book>, ) {
     }
 
+    findAll(): Promise<Book[]> {
+        return this.booksRepository.find();
+    }
 
+    findOne(id: string): Promise<Book> {
+        return this.booksRepository.findOne(id);
+    }
 
+    async remove(id: string): Promise<void> {
+        await this.booksRepository.delete(id);
+    }
 }
