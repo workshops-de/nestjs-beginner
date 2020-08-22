356ada7e6f35095292f02aaa631c8274ec4e3b30
robinboehm <robin.boehm@workshops.de>
2020-08-22 15:10:10 +0000

    chore(db): fix init

--- database/dump.sql
+++ database/dump.sql
@@ -5,7 +5,3 @@ CREATE TABLE `books` (`id` INT NOT NULL AUTO_INCREMENT,`name` TEXT,KEY `id` (`id
 INSERT INTO `books` (name) VALUES('NestJS Beginners');
 INSERT INTO `books` (name) VALUES('ReactJS Beginners');
 INSERT INTO `books` (name) VALUES('NodeJS Advanced Patterns');
-
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
-
-flush privileges;
--- database/setup.sh
+++ database/setup.sh
@@ -1,1 +1,4 @@
 cat dump.sql | mysql
+
+echo ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password' | mysql
+echo flush privileges; | mysql
