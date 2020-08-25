99ca39c063115c3621322ffedebf39ce0e4c0821
robinboehm <robin.boehm@workshops.de>
2020-08-25 07:57:35 +0000

    chore(setup): catch errors

--- database/setup.sh
+++ database/setup.sh
@@ -1,3 +1,3 @@
-cat dump.sql | mysql
+(cat dump.sql | mysql 2> /dev/null ) || echo Database already init
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';flush privileges;" | mysql
+(echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';flush privileges;" | mysql 2> /dev/null )  || echo Database User already setup
